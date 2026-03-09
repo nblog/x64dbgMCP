082398ef18e6351c29f59117db1a0790ebc2674e
nblog <503407184@qq.com>
2026-03-09 14:58:11 +0800

    实现 MCP 服务器

--- x64dbgMCP/plugintemplate/plugin.cpp
+++ x64dbgMCP/plugintemplate/plugin.cpp
@@ -15,56 +15,44 @@
 #define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
 #include "plugin.h"
+#include "../x64dbgMCP.h"
 
-// Examples: https://github.com/x64dbg/x64dbg/wiki/Plugins
 // References:
 // - https://help.x64dbg.com/en/latest/developers/plugins/index.html
-// - https://x64dbg.com/blog/2016/10/04/architecture-of-x64dbg.html
-// - https://x64dbg.com/blog/2016/10/20/threading-model.html
-// - https://x64dbg.com/blog/2016/07/30/x64dbg-plugin-sdk.html
 
-// Command use the same signature as main in C
-// argv[0] contains the full command, after that are the arguments
-// NOTE: arguments are separated by a COMMA (not space like WinDbg)
-static bool cbExampleCommand(int argc, char** argv)
+// mcp.start [port] - start MCP server on TCP loopback
+static bool cbMcpStart(int argc, char** argv)
 {
-    if (argc < 3)
-    {
-        dputs("Usage: " PLUGIN_NAME "expr1, expr2");
-
-        // Return false to indicate failure (used for scripting)
+    if (x64dbgMCP::McpServerHost::IsRunning) {
+        dputs("MCP server already running");
         return false;
     }
 
-    // Helper function for parsing expressions
-    // Reference: https://help.x64dbg.com/en/latest/introduction/Expressions.html
-    auto parseExpr = [](const char* expression, duint& value)
-    {
-        bool success = false;
-        value = DbgEval(expression, &success);
-        if (!success)
-            dprintf("Invalid expression '%s'\n", expression);
-        return success;
-    };
+    int port = 3001;
+    if (argc >= 2) {
+        port = atoi(argv[1]);
+        if (port <= 1024 || port > 49151) port = 3001;
+    }
 
-    duint a = 0;
-    if (!parseExpr(argv[1], a))
+    if (x64dbgMCP::McpServerHost::Start(port, nullptr)) {
+        dprintf("MCP server started on localhost:%d\n", port);
+        return true;
+    }
+
+    dputs("Failed to start MCP server");
+    return false;
+}
+
+// mcp.stop - stop MCP server
+static bool cbMcpStop(int argc, char** argv)
+{
+    if (!x64dbgMCP::McpServerHost::IsRunning) {
+        dputs("MCP server is not running");
         return false;
+    }
 
-    duint b = 0;
-    if (!parseExpr(argv[2], b))
-        return false;
-
-    // NOTE: Look at x64dbg-sdk/pluginsdk/bridgemain.h for a list of available functions.
-    // The Script:: namespace and DbgFunctions()->... are also good to check out.
-
-    // Do something meaningful with the arguments
-    duint result = a + b;
-    dprintf("$result = 0x%p + 0x%p = 0x%p\n", a, b, result);
-
-    // The $result variable can be used for scripts
-    DbgValToString("$result", result);
-
+    x64dbgMCP::McpServerHost::Stop();
+    dputs("MCP server stopped");
     return true;
 }
 
@@ -73,21 +61,17 @@ bool pluginInit(PLUG_INITSTRUCT* initStruct)
 {
     dprintf("pluginInit(pluginHandle: %d)\n", pluginHandle);
 
-    // Prefix of the functions to call here: _plugin_register
-    _plugin_registercommand(pluginHandle, PLUGIN_NAME, cbExampleCommand, false);
+    _plugin_registercommand(pluginHandle, "mcp.start", cbMcpStart, false);
+    _plugin_registercommand(pluginHandle, "mcp.stop", cbMcpStop, false);
 
     // Return false to cancel loading the plugin.
     return true;
 }
 
 // Deinitialize your plugin data here.
-// NOTE: you are responsible for gracefully closing your GUI
-// This function is not executed on the GUI thread, so you might need
-// to use WaitForSingleObject or similar to wait for everything to close.
 void pluginStop()
 {
-    // Prefix of the functions to call here: _plugin_unregister
-
+    x64dbgMCP::McpServerHost::Stop();
     dprintf("pluginStop(pluginHandle: %d)\n", pluginHandle);
 }
 
--- x64dbgMCP/x64dbgMCP.h
+++ x64dbgMCP/x64dbgMCP.h
@@ -1,10 +1,122 @@
 ﻿#pragma once
 
-using namespace System;
-
 namespace x64dbgMCP {
-	public ref class Class1
-	{
-		// TODO: 在此处为此类添加方法。
-	};
+
+    using namespace System;
+    using namespace System::IO;
+    using namespace System::Threading;
+    using namespace System::Threading::Tasks;
+    using namespace System::Collections::Generic;
+    using namespace Microsoft::AspNetCore::Builder;
+    using namespace Microsoft::Extensions::DependencyInjection;
+    using namespace ModelContextProtocol::Server;
+    using namespace ModelContextProtocol::Protocol;
+
+    public ref class McpServerHost
+    {
+    private:
+        static WebApplication^ _app;
+        static Task^ _serverTask;
+        static bool _running = false;
+        static int _port;
+		static String^ _httpUrl;
+
+        static void Log(String^ msg)
+        {
+            try {
+                Diagnostics::Debug::WriteLine(DateTime::Now.ToString("HH:mm:ss.fff") + " " + msg);
+            } catch (...) {}
+        }
+
+    public:
+        static property bool IsRunning { bool get() { return _running; } }
+
+		static bool Start(int port, String^ httpUrl)
+        {
+            if (_running) return false;
+            _port = port;
+			_httpUrl = httpUrl ? httpUrl : String::Format("http://localhost:{0}", port);
+            _serverTask = Task::Run(gcnew Action(&RunServerEntry));
+            _running = true;
+            return _running;
+        }
+
+        static void Stop()
+        {
+            if (!_running) return;
+            try {
+                if (_app != nullptr)
+                    _app->StopAsync()->GetAwaiter().GetResult();
+                if (_serverTask != nullptr)
+                    _serverTask->Wait(5000);
+            } catch (...) {}
+            _running = false;
+        }
+
+    private:
+        // simple echo tool for PoC validation
+        static String^ Echo(String^ message)
+        {
+            return "echo: " + message;
+        }
+
+        static void ConfigureMcpOptions(McpServerOptions^ opts)
+        {
+            auto asmName = Reflection::Assembly::GetExecutingAssembly()->GetName();
+            opts->ServerInfo = gcnew Implementation();
+            opts->ServerInfo->Name = asmName->Name;
+            opts->ServerInfo->Version = "1.0.0";
+        }
+
+        static void RunServerEntry()
+        {
+            try {
+                Log("Starting MCP HTTP server on port " + _port);
+
+                auto builder = WebApplication::CreateSlimBuilder();
+
+                // Register MCP server with options
+                auto mcpBuilder = McpServerServiceCollectionExtensions::AddMcpServer(
+                    builder->Services,
+                    gcnew Action<McpServerOptions^>(&ConfigureMcpOptions));
+
+                // Create and register echo tool
+                auto toolOpts = gcnew McpServerToolCreateOptions();
+                toolOpts->Name = "echo";
+                toolOpts->Description = "Echoes the input back";
+
+                auto echoTool = McpServerTool::Create(
+                    gcnew Func<String^, String^>(&McpServerHost::Echo),
+                    toolOpts);
+
+                auto tools = gcnew List<McpServerTool^>();
+                tools->Add(echoTool);
+                McpServerBuilderExtensions::WithTools(mcpBuilder,
+                    safe_cast<IEnumerable<McpServerTool^>^>(tools));
+
+                // Add HTTP transport (enables Streamable HTTP + legacy SSE)
+                HttpMcpServerBuilderExtensions::WithHttpTransport(mcpBuilder, nullptr);
+
+                _app = builder->Build();
+
+                // Listen on loopback only
+                _app->Urls->Add(_httpUrl);
+
+                // Map MCP endpoints:
+                //   Streamable HTTP: POST /
+                //   Legacy SSE:      GET /sse, POST /message
+                McpEndpointRouteBuilderExtensions::MapMcp(_app, "");
+
+                Log("WebApplication starting");
+                _app->RunAsync()->GetAwaiter().GetResult();
+            }
+            catch (Exception^ ex) {
+                Log("RunServer fatal: " + ex->ToString());
+            }
+            finally {
+                _running = false;
+                Log("RunServer exited");
+            }
+        }
+    };
 }
